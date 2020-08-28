9fbe153cb0322a5b493e1b99cf92c9d5299292db
Fintan O'Hea <fintanohea@gmail.com>
2020-08-27 20:20:42 -0700

    working snipcart example

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,20 +16,20 @@ describe('AppComponent', () => {
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
+    const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'sauna-king'`, () => {
+  it(`should have as title 'snipcart-angular-sanity'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('sauna-king');
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('snipcart-angular-sanity');
   });
 
-  it('should render title', () => {
+  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('sauna-king app is running!');
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to snipcart-angular-sanity!');
   });
 });
