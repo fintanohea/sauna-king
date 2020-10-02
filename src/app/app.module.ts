687829fd33e5975ff048d7e6e4f6a2ede355648c
Fintan O'Hea <fintanohea@gmail.com>
2020-10-02 12:43:55 -0700

    adding signup form to footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -8,17 +19,7 @@ import { ProductsListComponent } from './components/products-list/products-list.
 import { ProductsListItemComponent } from './components/products-list-item/products-list-item.component';
 import { AboutComponent } from './components/about/about.component';
 import { SteamcubeProductComponent } from './components/steamcube-product/steamcube-product.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatListModule } from '@angular/material/list';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatCardModule } from '@angular/material/card';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { ContactFormComponent } from './components/contact-form/contact-form.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
 import { HomepageComponent } from './components/homepage/homepage.component';
 import { ProductPageComponent } from './components/product-page/product-page.component';
 
@@ -47,7 +48,8 @@ import { ProductPageComponent } from './components/product-page/product-page.com
     MatFormFieldModule,
     FormsModule,
     ReactiveFormsModule,
-    MatInputModule
+    MatInputModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
